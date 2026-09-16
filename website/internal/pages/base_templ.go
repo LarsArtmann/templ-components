a89c8f1d6aa2bd6d89dcce719c85b2ff44702aca
Lars Artmann <git@lars.software>
2026-09-16 14:15:30 +0200

    chore: auto-commit 1 changed file(s) (heuristic)

--- website/internal/pages/base_templ.go
+++ website/internal/pages/base_templ.go
@@ -9,7 +9,7 @@ import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
 import (
-	"encoding/json/v2"
+	"encoding/json"
 
 	"github.com/larsartmann/templ-components/icons"
 	"github.com/larsartmann/templ-components/layout"
